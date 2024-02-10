3d970c0f692cd23638dea34b418fc45567946423
ree-boot <95701406+ree-boot@users.noreply.github.com>
2024-02-11 01:25:01 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 const contractJSON = require("../artifacts/contracts/NFT.sol/NFT.json");
 
-const contractAddress = "0x7c950A0f8cAcbf5CEc57C0B7B488baA6e1555B96"; // place your poly contract address here
+const contractAddress = "Address"; // place your polygon contract address here
 const contractABI = contractJSON.abi;
-const walletAddress = "0x69667b36755222864cfa8ACB3777a5842C4c8a51"; // place your public address for your wallet here
+const walletAddress = "Address"; // place the public address for your wallet here
 
 async function main() {
 
